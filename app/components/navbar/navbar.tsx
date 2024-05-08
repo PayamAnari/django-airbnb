06de0cd9793c94e95865f5bc5f5a12fb520c725d
PayamAnari <anari.p62@gmail.com>
2024-05-08 13:31:17 +0200

    Add searchfilters to navbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import SearchFilters from "./Searchfilters"
 
 const Navbar = () => {
   return (
@@ -16,7 +17,7 @@ const Navbar = () => {
           </Link>
 
           <div className="flex space-x-6">
-              Search Filter
+              <SearchFilters />
           </div>
           <div className="flex items-center space-x-6">
              Add property

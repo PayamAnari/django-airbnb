0648ea0f1bc882851bdfb5a9404e8b18b89bf1e4
PayamAnari <anari.p62@gmail.com>
2024-05-09 13:13:57 +0200

    Add categiries.tsx

--- app/components/navbar/AddPropertyButtton.tsx
+++ app/components/navbar/AddPropertyButtton.tsx
@@ -1,6 +1,6 @@
 const AddPropertyButton = () => {
   return (
-    <div className="p-2 text-sm font-semibold rounded-full hover:bg-gray-200">
+    <div className="p-2 cursor-pointer text-sm font-semibold rounded-full hover:bg-gray-200">
        Djangobnb your home
     </div>
   )

35ef429732c5c65ce884d419135c2a75e82b90e3
PayamAnari <anari.p62@gmail.com>
2024-05-09 12:19:59 +0200

    Add items

--- app/components/navbar/Searchfilters.tsx
+++ app/components/navbar/Searchfilters.tsx
@@ -7,6 +7,18 @@ const SearchFilters = () => {
               <p className="text-xs font-semibold">Where</p>
               <p className="text-sm">Wanted location</p>
            </div>
+           <div className="w-[250px] h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+              <p className="text-xs font-semibold">Check in</p>
+              <p className="text-sm">Add dates</p>
+           </div>
+           <div className="w-[250px] h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+              <p className="text-xs font-semibold">Check out</p>
+              <p className="text-sm">Add dates</p>
+           </div>
+           <div className="w-[250px] h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+              <p className="text-xs font-semibold">Who</p>
+              <p className="text-sm">Add guests</p>
+           </div>
         </div>
        </div>
     </div>

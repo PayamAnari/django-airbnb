b91ae0daf564683fec00d022f6dfc11e22f9b0f7
PayamAnari <anari.p62@gmail.com>
2024-05-10 14:46:24 +0200

    Modify propertylistitem

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,6 +1,18 @@
+import Image from "next/image";
+
 const PropertyListItem = () => {
   return (
-     <p>Item</p>
+     <div className="cursor-pointer">
+        <div className="overflow-hidden aspect-square rounded-xl">
+        <Image
+                    fill
+                    src="/images/beach-house.jpg"
+                    sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
+                    className="hover:scale-110 object-cover transition h-full w-full"
+                    alt="Beach house"
+                />
+        </div>
+     </div>
   )
 }
 

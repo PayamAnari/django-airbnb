4c3f1b308f476360aa34a96458622c26fe2e5914
PayamAnari <anari.p62@gmail.com>
2024-05-10 14:53:18 +0200

    Add title and price to property

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -3,14 +3,20 @@ import Image from "next/image";
 const PropertyListItem = () => {
   return (
      <div className="cursor-pointer">
-        <div className="overflow-hidden aspect-square rounded-xl">
+        <div className="relative overflow-hidden aspect-square rounded-xl">
         <Image
                     fill
-                    src="/images/beach-house.jpg"
+                    src="/beach_1.jpg"
                     sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
                     className="hover:scale-110 object-cover transition h-full w-full"
                     alt="Beach house"
                 />
+        </div>
+        <div className="mt-2">
+           <p className="text-lg font-bold">Property Name</p>
+        </div>
+        <div className="mt-2">
+          <p className="text-sm"><strong>$200</strong></p>
         </div>
      </div>
   )

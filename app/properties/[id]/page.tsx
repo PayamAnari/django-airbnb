31b28f1a290ee2414d44fb4ffcf6a99c56ce6bdd
PayamAnari <anari.p62@gmail.com>
2024-05-10 15:06:21 +0200

    Add image to page detail

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,6 +1,18 @@
+import Image from "next/image";
+
+
 const PropertyDetailPage = () => {
   return (
-    <p>hgfdrtdytfuygiuhoij</p>
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+         <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
+                <Image
+                    fill
+                    src="/beach_1.jpg"
+                    className="object-cover w-full h-full"
+                    alt="Beach house"
+                />
+            </div>
+    </main>
   )
 }
 

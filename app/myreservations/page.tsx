7f0e47a8583bd8c61a6f206ec215caa373212cf5
PayamAnari <anari.p62@gmail.com>
2024-05-11 21:36:02 +0200

    Correction

--- app/myreservations/page.tsx
+++ app/myreservations/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 const MyReservationsPage = () => {
   return (
-    <main className="max-w-[1500px] mx-auto px-6 pd-6">
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
       <h1 className="my-6 text-2xl">My reservations</h1>
 
-      <div>
+      <div className="space-y-4">
         <div className="p-5 grid grid-cols-1 md:grid-cols-4 gap-4 shadow-md border border-gray-300 rounded-xl">
             <div className="col-span-1">
                <div className="relative overflow-hidden aspect-square rounded-xl">
@@ -18,13 +18,17 @@ const MyReservationsPage = () => {
                />
                </div>
             </div>
-            <div className="col-span-3 space-y-2">
+            <div className="col-span-1 md:col-span-3">
                <h2 className="mb-4 text-xl">Property Name</h2>
 
-               <p><strong>Check in date:</strong> 15/04/2024</p>
-               <p><strong>Check out date:</strong> 05/09/2024</p>
+               <p className="mb-2"><strong>Check in date:</strong> 15/04/2024</p>
+               <p className="mb-2"><strong>Check out date:</strong> 05/09/2024</p>
+
+               <p className="mb-2"><strong>Number of nights:</strong> 7</p>
+               <p className="mb-2"><strong>Total price:</strong> $220</p>
             </div>
         </div>
+    
       </div>
     </main>
   )

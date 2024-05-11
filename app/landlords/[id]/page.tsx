7eb84447dbe6301a7214438c82b5e5a62cbba357
PayamAnari <anari.p62@gmail.com>
2024-05-11 20:40:10 +0200

    Modify page

--- app/landlords/[id]/page.tsx
+++ app/landlords/[id]/page.tsx
@@ -1,7 +1,18 @@
 const LandlordDetailPage = () => {
   return (
     <main className="max-w-[1500px] mx-auto px-6 pd-6">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+        <aside className="col-span-1 mb-4">
+          <div className="flex flex-col items-center p-6 rounded-xl border border-gray-300 shadow-xl">
 
+          </div>
+           ytre
+        </aside>
+
+        <div className="col-span-3 pl-0 md:pl-6">
+           properties
+        </div>
+      </div>
     </main>
 
   )

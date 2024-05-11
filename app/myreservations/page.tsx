1ac5f979d7a52c40034c5c4546384a7d9078bc6d
PayamAnari <anari.p62@gmail.com>
2024-05-11 21:05:57 +0200

    Modify myreservations

---
+++ app/myreservations/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+
+const MyReservationsPage = () => {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pd-6">
+      <h1 className="my-6 text-2xl">My reservations</h1>
+    </main>
+  )
+}
+
+
+export default MyReservationsPage

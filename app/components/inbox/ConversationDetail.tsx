03d43dd7ee444f5a3888216a0cea2fffb5af86e2
PayamAnari <anari.p62@gmail.com>
2024-05-12 20:36:09 +0200

    Modify conversation detail

--- app/components/inbox/ConversationDetail.tsx
+++ app/components/inbox/ConversationDetail.tsx
@@ -1,6 +1,12 @@
 const ConversationDetail = () => {
   return (
-    <div>gfds</div>
+    <div className="max-h-[400px] overflow-auto flex flex-col space-y-4">
+      <div className="w-[80%] py-4 px-6 rounded-xl bg-gray-200">
+         <p className="font-bold text-gray-500">John Doe</p>
+         <p>hgdfvdc tgrfdf thgrfeds hytgrfedws fdf</p>
+      </div>
+
+    </div>
   )
 }
 

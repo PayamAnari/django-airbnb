bc46e8744d625593952807e056a3446e13e3ee6c
PayamAnari <anari.p62@gmail.com>
2024-05-12 20:21:27 +0200

    Modify page inbox

--- app/components/inbox/conversation.tsx
+++ app/components/inbox/conversation.tsx
@@ -1,8 +1,8 @@
 const Conversation = () => {
   return (
-    <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl">
-    <p className="mb-6 text-xl"></p>
-
+    <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl shadow-lg">
+    <p className="mb-6 text-xl">John Doe</p>
+    
     <p 
         className="text-airbnb-dark"
     >
---
+++ app/inbox/[id]/page.tsx
@@ -0,0 +1,9 @@
+const ConversationPage = () => {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+      ffff
+    </main>
+  )
+}
+
+export default ConversationPage;

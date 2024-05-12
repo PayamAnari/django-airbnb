a1d9ce8a7044af37bfb1e535a69ddabf0121a97e
PayamAnari <anari.p62@gmail.com>
2024-05-12 20:10:09 +0200

    Create inbox folder

--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -1,9 +1,8 @@
 const InboxPage = () => {
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4">
-    <h1 className="my-6 text-2xl">Inbox</h1>
-
-</main>
+       <h1 className="my-6 text-2xl">Inbox</h1>
+    </main>
   )
 } 
 

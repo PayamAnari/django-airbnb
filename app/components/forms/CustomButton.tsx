9a4540228b5da461e9bbcd41e1d9fa66b3935bde
PayamAnari <anari.p62@gmail.com>
2024-05-12 20:51:22 +0200

    Add custombutton.tsx

---
+++ app/components/forms/CustomButton.tsx
@@ -0,0 +1,10 @@
+const CustomButton = () => {
+  return (
+    <div 
+    className="w-full py-4 bg-airbnb hover:bg-airbnb-dark text-white text-center rounded-xl transition cursor-pointer">
+
+   </div>
+  )
+}
+
+export default CustomButton;

9cc062bb09ca91478b29dab25068b01cab74bf32
PayamAnari <anari.p62@gmail.com>
2024-05-13 16:28:32 +0200

    Add signupmodal to usernav

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -3,10 +3,12 @@
 import { useState } from "react";
 import MenuLink from "./MenuLink";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import useSignupModal from "@/app/hooks/useSignUpModal";
 
 
 const UserNav = () => {
   const loginModal = useLoginModal();
+  const signupModal = useSignupModal();
   const [isOpen, setIsOpen] = useState(false)
 
   return (
@@ -36,7 +38,10 @@ const UserNav = () => {
 
             <MenuLink
                label="Sign up"
-               onClick={() => console.log("Clicked")}
+               onClick={() => {console.log("Clicked")
+                setIsOpen(false);
+                signupModal.open()
+              }}
             />
           </div>
        )}

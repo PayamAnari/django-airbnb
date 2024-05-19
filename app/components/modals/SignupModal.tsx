c5c07b95637705141bfe2c9161fe5f35043f4c08
PayamAnari <anari.p62@gmail.com>
2024-05-19 15:38:47 +0200

    add submitsignup func

--- app/components/modals/SignupModal.tsx
+++ app/components/modals/SignupModal.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import useSignupModal from "@/app/hooks/useSignUpModal";
 import CustomButton from "../forms/CustomButton";
+import apiService from "@/app/services/apiService";
 
 
 const SignupModal = () => {
@@ -16,10 +17,33 @@ const SignupModal = () => {
   const [errors, setErrors] = useState<string[]>([]); 
 
 
+  const submitSignup = async () => {
+    const formData = {
+      email: email,
+      password1: password1,
+      password2: password2
+    }
+
+    const response = await apiService.post("/api/auth/register/", formData);
+
+    if(response.access) {
+      signupModal.close();
+      router.push("/");
+    } else {
+      const tmpErrors: string[] = Object.values(response).map((error: any) => {
+        return error[0];
+      
+      })
+    }
+  }
+
+
   const content = (
     <>
     <h2 className="mb-6 text-2xl">Welcome to Djangobnb, please signup</h2>
-    <form className="space-y-4">
+    <form 
+      action={submitSignup}
+      className="space-y-4">
       <input onChange={(e) => setEmail(e.target.value)} placeholder="Your email address" type="e-mail" 
       className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
        <input onChange={(e) => setPassword1(e.target.value)} placeholder="Your password" type="password" 
@@ -29,8 +53,10 @@ const SignupModal = () => {
       
       {errors.map((error, index) => {
         return (
-          <div className="p-5 bg-airbnb text-white rounded-xl opacity-70">
-          Error message
+          <div 
+          key={`error_${index}`}
+          className="p-5 bg-airbnb text-white rounded-xl opacity-70">
+             {error}
        </div>
         )
       })}

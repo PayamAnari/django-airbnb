87446803fdb63a060970f18fbaaff8fb81609a5f
PayamAnari <anari.p62@gmail.com>
2024-05-19 16:31:14 +0200

    import handlelogin to signup

--- app/components/modals/SignupModal.tsx
+++ app/components/modals/SignupModal.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import useSignupModal from "@/app/hooks/useSignUpModal";
 import CustomButton from "../forms/CustomButton";
 import apiService from "@/app/services/apiService";
+import { handleLogin } from "@/app/lib/actions";
 
 
 const SignupModal = () => {
@@ -27,6 +28,9 @@ const SignupModal = () => {
     const response = await apiService.post("/api/auth/register/", JSON.stringify(formData));
 
     if(response.access) {
+
+      handleLogin(response.user.pk, response.access, response.refresh);
+
       signupModal.close();
       router.push("/");
     } else {
--- app/lib/action.ts
+++
@@ -1,6 +0,0 @@
-'user server';
-
-import { cookies } from 'next/headers';
-
-
-export async function handleLogin(userId: string, accessToken: string, refreshToken: string)

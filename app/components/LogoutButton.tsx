638901a45098ea4f5b2272ea60c4c353a79d18d3
PayamAnari <anari.p62@gmail.com>
2024-05-20 15:49:08 +0200

    Modify logout button

--- app/components/LogoutButton.tsx
+++ app/components/LogoutButton.tsx
@@ -2,3 +2,22 @@
 
 import { useRouter } from "next/navigation";
 import MenuLink from "./navbar/MenuLink";
+
+
+const LogoutButton = () => {
+  const router = useRouter()
+
+  const submitLogout = async () => {
+    router.push("/")
+  }
+
+  return (
+    <MenuLink
+        label="Log out"
+        onClick={submitLogout}
+    />
+  )
+}
+
+
+export default LogoutButton;

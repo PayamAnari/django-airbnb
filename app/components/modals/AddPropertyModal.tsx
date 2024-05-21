15191634da45ccbffd361730f6ba916c3401bf5b
PayamAnari <anari.p62@gmail.com>
2024-05-21 21:18:25 +0200

    Modify addpropertymodal

--- app/components/modals/AddPropertyModal.tsx
+++ app/components/modals/AddPropertyModal.tsx
@@ -2,8 +2,23 @@
 
 import Image from "next/image";
 import Modal from "./Modal";
+import useAddPropertyModal from "@/app/hooks/usePropertyModal";
 
 
 const AddPropertyModal = () => {
+  const addPorpertyModal = useAddPropertyModal();
 
+  return (
+    <>
+       <Modal 
+        isOpen={addPorpertyModal.isOpen}
+        close={addPorpertyModal.close}
+        label="Add Property"
+        content={(
+          <p>Yo</p>
+        )}
+       
+       />
+    </>
+  )
 }
--- app/hooks/usePropertyModal.ts
+++ app/hooks/usePropertyModal.ts
@@ -1,15 +1,15 @@
 import { create } from 'zustand';
 
-interfaceAddPropertyModalStore {
+interface AddPropertyModalStore {
   isOpen: boolean;
   open: () => void;
   close: () => void;
 }
 
-const usePropertyModal = create<AddPropertyModalStore>((set) => ({
+const useAddPropertyModal = create<AddPropertyModalStore>((set) => ({
   isOpen: false,
   open: () => set({ isOpen: true }),
   close: () => set({ isOpen: false }),
 }));
 
-export default usePropertyModal;
+export default useAddPropertyModal;

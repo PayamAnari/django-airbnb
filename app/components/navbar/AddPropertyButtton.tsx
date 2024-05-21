a28d7195a38f58ec788d6f04c3907031d20a522b
PayamAnari <anari.p62@gmail.com>
2024-05-21 21:25:01 +0200

    modify addpropertybutton

--- app/components/navbar/AddPropertyButtton.tsx
+++ app/components/navbar/AddPropertyButtton.tsx
@@ -1,6 +1,11 @@
 import useAddPropertyModal from "@/app/hooks/useAddPropertyModal";
 
 const AddPropertyButton = () => {
+  const addPropertyModal = useAddPropertyModal();
+  const airbnbYourHome = () => {
+    addPropertyModal.open();
+  
+  }
   return (
     <div className="p-2 cursor-pointer text-sm font-semibold rounded-full hover:bg-gray-200">
        Djangobnb your home

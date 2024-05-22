e550bf1a9a60f1eb3915225522b6fd8b1b7944eb
PayamAnari <anari.p62@gmail.com>
2024-05-22 21:43:16 +0200

    Add design image

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -18,7 +18,7 @@ const Categories = () => {
     <div 
         className={`pb-4 flex flex-col items-center space-y-2 border-b-2  opacity-60 hover:border-gray-200 hover:opacity-100`}>
         <Image
-            src="/icn_category_beach.jpeg"
+            src="/beach.jpeg"
             alt="Category - Beach"
             width={20}
             height={20}
@@ -30,13 +30,13 @@ const Categories = () => {
     <div 
         className={`pb-4 flex flex-col items-center space-y-2 border-b-2  opacity-60 hover:border-gray-200 hover:opacity-100`}>
         <Image
-            src="/icn_category_beach.jpeg"
+            src="/design.jpeg"
             alt="Category - Beach"
             width={20}
             height={20}
         />
 
-        <span className='text-xs'>Villas</span>
+        <span className='text-xs'>Design</span>
     </div>
 
     <div 

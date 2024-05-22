25ed5d34fb8c58c89d43fe8e80100ed7b5748145
PayamAnari <anari.p62@gmail.com>
2024-05-22 21:21:33 +0200

    Add categories

--- app/components/modals/addproperty/Categories.tsx
+++ app/components/modals/addproperty/Categories.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+
+
+
 interface CategoriesProps {
   dataCategory: string;
   setCategory: (category: string) => void;
@@ -10,7 +14,66 @@ const Categories: React.FC<CategoriesProps> = ({
   return (
     <>
     <div className="pt-3 cursor-pointer pb-6 item_center space-x-12">
-      gfds
+
+    <div 
+        className={`pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100`}>
+        <Image
+            src="/icn_category_beach.jpeg"
+            alt="Category - Beach"
+            width={20}
+            height={20}
+        />
+
+        <span className='text-xs'>All</span>
+    </div>
+    
+    <div 
+        className={`pb-4 flex flex-col items-center space-y-2 border-b-2  opacity-60 hover:border-gray-200 hover:opacity-100`}>
+        <Image
+            src="/icn_category_beach.jpeg"
+            alt="Category - Beach"
+            width={20}
+            height={20}
+        />
+
+        <span className='text-xs'>Beach</span>
+    </div>
+
+    <div 
+        className={`pb-4 flex flex-col items-center space-y-2 border-b-2  opacity-60 hover:border-gray-200 hover:opacity-100`}>
+        <Image
+            src="/icn_category_beach.jpeg"
+            alt="Category - Beach"
+            width={20}
+            height={20}
+        />
+
+        <span className='text-xs'>Villas</span>
+    </div>
+
+    <div 
+        className={`pb-4 flex flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100`}>
+        <Image
+            src="/icn_category_beach.jpeg"
+            alt="Category - Beach"
+            width={20}
+            height={20}
+        />
+
+        <span className='text-xs'>Cabins</span>
+    </div>
+
+    <div
+        className={`pb-4 flex flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100`}>
+        <Image
+            src="/icn_category_beach.jpeg"
+            alt="Category - Beach"
+            width={20}
+            height={20}
+        />
+
+        <span className='text-xs'>Tiny homes</span>
+    </div>
     </div>
     </>
   )

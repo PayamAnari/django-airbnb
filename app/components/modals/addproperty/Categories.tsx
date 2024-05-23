9be0308db9c69a4fa4cfe07ce8bce0f12a72a8ea
PayamAnari <anari.p62@gmail.com>
2024-05-23 21:30:18 +0200

    Add onclick to cabins

--- app/components/modals/addproperty/Categories.tsx
+++ app/components/modals/addproperty/Categories.tsx
@@ -54,6 +54,7 @@ const Categories: React.FC<CategoriesProps> = ({
     </div>
 
     <div 
+        onClick={() => setCategory("Cabins")}
         className={`pb-4 flex flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100`}>
         <Image
             src="/cabns.jpeg"

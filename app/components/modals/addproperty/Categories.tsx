c82e7556ecbec6f12184c3417adbfc74189f70c8
PayamAnari <anari.p62@gmail.com>
2024-05-23 21:30:41 +0200

    Add onclick to tinyhomes

--- app/components/modals/addproperty/Categories.tsx
+++ app/components/modals/addproperty/Categories.tsx
@@ -67,6 +67,7 @@ const Categories: React.FC<CategoriesProps> = ({
     </div>
 
     <div
+        onClick={() => setCategory("Tiny homes")}
         className={`pb-4 flex flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100`}>
         <Image
             src="/tiny homes.jpeg"

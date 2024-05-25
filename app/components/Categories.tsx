c9714f90bf31d045429cbf8ecb46325bda478f63
PayamAnari <anari.p62@gmail.com>
2024-05-25 16:50:36 +0200

    Correction

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -4,7 +4,7 @@ import CategoryData from "./CategoryData";
 const Categories = () => {
 
   return (
-    <div className="pt-3 pb-6">
+    <div className="pt-3 pb-6 mx-10 md:mx-12 lg:mx-16">
       <Carousel data={CategoryData} />
     </div>
   )

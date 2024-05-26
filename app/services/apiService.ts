3d7b5a39b8b66aca90d4b3c81ca8d49d0ac67eb3
PayamAnari <anari.p62@gmail.com>
2024-05-26 12:26:54 +0200

    Add postwithout token

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -4,7 +4,7 @@ const apiService = {
   get: async function (url: string): Promise<any> {
     console.log('get', url);
 
-    const token = getAccessToken();
+    const token = await getAccessToken();
 
     return new Promise((resolve, reject) => {
       fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
@@ -30,6 +30,8 @@ const apiService = {
   post: async function (url: string, data: any): Promise<any> {
     console.log('post', url, data);
 
+    const token = await getAccessToken();
+
     return new Promise((resolve, reject) => {
       fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
         method: 'POST',
@@ -37,6 +39,7 @@ const apiService = {
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
         },
       })
         .then((response) => response.json())
@@ -50,6 +53,32 @@ const apiService = {
         });
     });
   },
-};
+},
+
+   postWithoutToken: async function(url: string, data: any): Promise<any> {
+        console.log('post', url, data);
+
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'POST',
+                body: data,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then((json) => {
+                    console.log('Response:', json);
+
+                    resolve(json);
+                })
+                .catch((error => {
+                    reject(error);
+                }))
+        })
+    }
+}
+  
 
 export default apiService;

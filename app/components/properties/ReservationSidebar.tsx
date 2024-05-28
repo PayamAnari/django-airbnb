85feba1e9e25bafaaace3f4b9856670299b5bcf3
PayamAnari <anari.p62@gmail.com>
2024-05-28 13:01:01 +0200

    Add onchange

--- app/components/properties/ReservationSidebar.tsx
+++ app/components/properties/ReservationSidebar.tsx
@@ -42,12 +42,10 @@ const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
       <h2 className="mb-5 text-2xl">${property.price_per_night} per night</h2>
       <div className="mb-6 p-3 border border-gray-400 rounded-xl">
         <label className="mb-2 block font-bold text-xs">Guests</label>
-         <select className="w-full -ml-1 text-xm">
-           <option>1</option>
-           <option>2</option>
-           <option>3</option>
-           <option>4</option>
-           <option>5</option>
+         <select 
+         value={guests}
+         onChange={(e) => setGuests(e.target.value)}
+         className="w-full -ml-1 text-xm">
          </select>
       </div>
 

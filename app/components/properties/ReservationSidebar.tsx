acca9a5d7500e5861f590325f8231944e6828931
PayamAnari <anari.p62@gmail.com>
2024-05-28 21:56:24 +0200

    Add datepicker to reservation

--- app/components/properties/ReservationSidebar.tsx
+++ app/components/properties/ReservationSidebar.tsx
@@ -63,6 +63,12 @@ const ReservationSidebar: React.FC<ReservationSidebarProps> = ({
   return (
     <aside className="mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
       <h2 className="mb-5 text-2xl">${property.price_per_night} per night</h2>
+
+      <DatePicker
+      value={dateRange}
+      onChange={(value) => _setDateRange(value.selection)}
+      />
+
       <div className="mb-6 p-3 border border-gray-400 rounded-xl">
         <label className="mb-2 block font-bold text-xs">Guests</label>
          <select 

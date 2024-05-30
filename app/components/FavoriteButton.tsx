780d84988cfb6aff5b21214d80d77b76579ed658
PayamAnari <anari.p62@gmail.com>
2024-05-30 15:50:23 +0200

    modify favoritebutton

--- app/components/FavoriteButton.tsx
+++ app/components/FavoriteButton.tsx
@@ -7,3 +7,16 @@ interface FavoriteButtonProps {
   is_favorite: boolean;
   markFavorite: (is_favorite: boolean) => void;
 }
+
+const FavoriteButton: React.FC<FavoriteButtonProps> = ({
+  id,
+  is_favorite,
+  markFavorite
+}) => {
+  return (
+
+  )
+}
+
+
+export default FavoriteButton;

e3c6362f41d81ed737a0fafec5da07ecbe96033c
PayamAnari <anari.p62@gmail.com>
2024-05-30 13:26:11 +0200

    Modify my reservations

--- app/myreservations/page.tsx
+++ app/myreservations/page.tsx
@@ -1,38 +1,42 @@
 import Image from "next/image";
+import apiService from "../services/apiService";
 
 
-const MyReservationsPage = () => {
+const MyReservationsPage = async () => {
+  const reservations = await apiService.get("/api/auth/myreservations/");
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6">
       <h1 className="my-6 text-2xl">My reservations</h1>
 
       <div className="space-y-4">
+         {reservations.map((reservation: any) => {
+
         <div className="p-5 grid grid-cols-1 md:grid-cols-4 gap-4 shadow-md border border-gray-300 rounded-xl">
             <div className="col-span-1">
                <div className="relative overflow-hidden aspect-square rounded-xl">
                <Image
                   fill
-                  src="/beach_1.jpg"
+                  src={reservations.property.image_url}
                   className="hover:scale-110 object-cover transition h-full w-full"
                   alt="Beach house"
                />
                </div>
             </div>
             <div className="col-span-1 md:col-span-3">
-               <h2 className="mb-4 text-xl">Property Name</h2>
+               <h2 className="mb-4 text-xl">{reservation.property.title}</h2>
 
-               <p className="mb-2"><strong>Check in date:</strong> 15/04/2024</p>
-               <p className="mb-2"><strong>Check out date:</strong> 05/09/2024</p>
+               <p className="mb-2"><strong>Check in date:</strong> {reservation.start_date}</p>
+               <p className="mb-2"><strong>Check out date:</strong> {reservation.end_date}</p>
 
-               <p className="mb-2"><strong>Number of nights:</strong> 7</p>
-               <p className="mb-2"><strong>Total price:</strong> $220</p>
+               <p className="mb-2"><strong>Number of nights:</strong> {reservation.number_of_nights}</p>
+               <p className="mb-2"><strong>Total price:</strong> ${reservation.total_price}</p>
 
                <div className="mt-6 inline-block cursor-pointer py-4 px-6 bg-airbnb hover:bg-airbnb-dark transition text-white rounded-xl">
                Go to property
                </div>
             </div>
         </div>
-    
+       })}
       </div>
     </main>
   )

f9a6376e99abb229121eed29c15282a9a72230b2
PayamAnari <anari.p62@gmail.com>
2024-06-04 22:04:19 +0200

    Modify conversation

--- app/components/inbox/conversation.tsx
+++ app/components/inbox/conversation.tsx
@@ -1,4 +1,18 @@
-const Conversation = () => {
+"use client"
+
+import { useRouter } from "next/navigation";
+import { ConversationType } from "@/app/inbox/page";
+
+interface ConversationProps {
+  conversation: ConversationType;
+  userId: string;
+}
+
+const Conversation: React.FC<ConversationProps> = ({
+  conversation,
+  userId
+}) => {
+  const router = useRouter();
   return (
     <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl shadow-lg">
     <p className="mb-6 text-xl">John Doe</p>
--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -24,13 +24,21 @@ const InboxPage = async () => {
       </main>
     )
   }
+
+  const conversations = await apiService.get("/api/chat/")
+
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4">
        <h1 className="my-6 text-2xl">Inbox</h1>
-
-       <Conversation />
-       <Conversation />
-       <Conversation />
+      {conversations.map((conversation: ConversationType) => {
+        return (
+          <Conversation
+             userId={userId}
+             conversation={conversation}
+          />
+        )
+      })}
+      
     </main>
   )
 } 

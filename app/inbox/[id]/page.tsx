91c70b0d1b57d9e2d8bd4784ce8a3c78f5f6452d
PayamAnari <anari.p62@gmail.com>
2024-06-05 13:33:41 +0200

    Modify inbox id

--- app/inbox/[id]/page.tsx
+++ app/inbox/[id]/page.tsx
@@ -1,6 +1,16 @@
 import ConversationDetail from "@/app/components/inbox/ConversationDetail";
+import { getUserId } from "@/app/lib/actions";
+import React, { useState, useEffect } from "react";
+import apiService from "@/app/services/apiService";
 
-const ConversationPage = () => {
+const ConversationPage = async () => {
+  const userId = getUserId();
+
+  if (!userId) {
+     <main className="max-w-[1500px] max-auto px-6 py-12">
+        <p>You need to be authenticated ...</p>
+     </main>
+  }
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6">
       <ConversationDetail/>

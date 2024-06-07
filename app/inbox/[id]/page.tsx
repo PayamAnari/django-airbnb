c80202f2b6caa1ba01f764fd5d06e4b2a0ab4c8e
PayamAnari <anari.p62@gmail.com>
2024-06-07 22:30:05 +0200

    Correction

--- app/inbox/[id]/page.tsx
+++ app/inbox/[id]/page.tsx
@@ -26,6 +26,7 @@ const ConversationPage = async ({ params }: { params: {id: string }}) => {
             </main>
         )
     }
+    
 
     const conversation = await apiService.get(`/api/chat/${params.id}/`)
 

b272f09b8da93d95f38974f11b88972b9f191f7c
PayamAnari <anari.p62@gmail.com>
2024-06-07 21:22:15 +0200

    Add profile photo to inbox users

--- app/components/inbox/conversation.tsx
+++ app/components/inbox/conversation.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { ConversationType } from "@/app/inbox/page";
+import Image from "next/image";
 
 interface ConversationProps {
   conversation: ConversationType;
@@ -16,13 +17,24 @@ const Conversation: React.FC<ConversationProps> = ({
   const otherUser = conversation.users.find((user) => user.id != userId);
 
   return (
-    <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl shadow-lg">
-    <p className="mb-6 text-xl"
-      onClick={() => router.push(`/landlords/${otherUser?.id}`)}
-    >
-      {otherUser?.name}
-    </p>
-    
+    <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl shadow-lg">  
+        <div className="flex gap-2">
+        {otherUser?.avatar_url && (
+
+         <Image
+            src={otherUser.avatar_url}
+            width={30}
+            height={30}
+            alt="Landlord image"
+            className="rounded-full ml-2"
+          />
+              )}
+       <p className="text-xl"
+       onClick={() => router.push(`/landlords/${otherUser?.id}`)}
+      >
+       {otherUser?.name}
+       </p>
+    </div>
     <p 
         onClick={() => router.push(`/inbox/${conversation.id}`)}
         className="text-airbnb-dark"

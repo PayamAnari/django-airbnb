8287710725bcaa83d77a2e0fe16547617142f799
PayamAnari <anari.p62@gmail.com>
2024-06-10 21:02:43 +0200

    Import usesearchmodal to categories

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -1,7 +1,14 @@
+"use client"
+
+import { useState } from "react";
+import useSearchModal, {SearchQuery} from "../hooks/useSearchModal";
 import Carousel from "./Carousel";
 import CategoryData from "./forms/CategoryData";
 
 const Categories = () => {
+
+  const searchModal = useSearchModal();
+  const[category, setCategory] = useState("");
 
   return (
     <div className="pt-3 pb-6 mx-10 md:mx-12 lg:mx-16">

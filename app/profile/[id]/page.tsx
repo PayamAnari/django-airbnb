fe2186d95f1aae154feebe706bc7c769d45f6247
PayamAnari <anari.p62@gmail.com>
2024-06-14 11:50:52 +0200

    Modify profile

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -184,19 +184,21 @@ const LandlordProfilePage = ({ params }: { params: { id: string }}) => {
                 onChange={handleChange}
                 className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-airbnb appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-airbnb-dark peer" placeholder=""
               />
-              <label htmlFor="floating_filled" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-airbnb peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Address:</label>
+              <label htmlFor="floating_filled" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-airbnb peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Address</label>
 
               </div>
-            
-              <label>Favorite song:</label>
+              
+              <div className="relative">
               <input
                 type="text"
                 name="favorite_song"
-                placeholder="Favorite Song"
                 value={formData.favorite_song || ''}
                 onChange={handleChange}
-                className="px-4 py-2 border border-gray-300 rounded-lg"
+                className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-airbnb appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-airbnb-dark peer" placeholder=""
               />
+              <label htmlFor="floating_filled" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-airbnb peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Favorite song</label>
+
+              </div>
                 <div className="relative">
               <textarea
                 name="about_me"

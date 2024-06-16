a131639b8894fd6b1548de02aa7a2735a24e6285
PayamAnari <anari.p62@gmail.com>
2024-06-16 21:47:33 +0200

    Modify

--- app/deleteproperty/[id]/page.tsx
+++ app/deleteproperty/[id]/page.tsx
@@ -2,6 +2,30 @@
 
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
+import apiService from "@/app/services/apiService";
 
 
+const deleteProperty = async ({ params }: { params: { id: string } }) => {
 
+  const router = useRouter();
+  
+  try {
+    const response = await apiService.deleteProperty(`/api/properties/${params.id}/`);
+    if (response.success) {
+      toast.success("Property deleted successfully!", {
+        position: "top-center",
+        autoClose: 2000,
+      });
+      router.push("/?deleted=true");
+    } else {
+      toast.error("Failed to delete property. Please try again.", {
+        position: "top-center",
+      });
+    }
+  } catch (error) {
+    console.error("Failed to delete property", error);
+    toast.error("Failed to delete property. Please try again.");
+  }
+};
+
+<button onClick={() => deleteProperty(propertyId)}>Delete Property</button>

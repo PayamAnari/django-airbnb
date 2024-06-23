9841f139a658d1d3d6bf218251dbd2293abe53df
PayamAnari <anari.p62@gmail.com>
2024-06-23 23:03:46 +0200

    Modify payment

--- app/payment/[id]/page.tsx
+++ app/payment/[id]/page.tsx
@@ -1,9 +1,59 @@
-const PaymentService = () => {
-  return (
-    <div>
-      <h1>Payment Service</h1>
-    </div>
+"use client"
+
+
+import usePaymentModal from "@/app/hooks/usePaymentModal";
+import Modal from "@/app/components/modals/Modal";
+import CustomButton from "@/app/components/forms/CustomButton";
+import Link from "next/link";
+import apiService from "@/app/services/apiService";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from 'react';
+
+
+
+const PaymentService = async ({ params }: { params: { id: string }}) => {
+  const paymentModal = usePaymentModal();
+  const router = useRouter();
+  const [reservation, setReservation] = useState(null);
+
+  useEffect(() => {
+    const fetchReservation = async () => {
+      if (params.id) {
+        const data = await apiService.get(`/api/auth/${params.id}/myreservations`);
+        setReservation(data);
+      }
+    };
+    fetchReservation();
+  }, [params.id]);
+  
+
+  
+  const content = (
+
+    <>
+     <h1>Payment</h1>
+    
+    <Link href={`payment/`}>
+     <CustomButton 
+      label="Payment"
+      onClick={paymentModal.close}
+      className=""
+      />
+    </Link>
+    </>
   );
+
+  return (
+    <>
+       <Modal 
+        isOpen={paymentModal.isOpen}
+        close={paymentModal.close}
+        label="Edit Property"
+        content={content}
+       
+       />
+    </>
+  )
 }
 
 

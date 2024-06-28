60e469acbf678053f5d681ec84f62ea87d46d4e2
PayamAnari <anari.p62@gmail.com>
2024-06-28 19:23:26 +0200

    Create delete reservation

---
+++ app/deletereservation/[id]/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { toast } from "react-toastify";
+import apiService from "@/app/services/apiService";
+
+
+const deleteReservation = async ( id: string ) => {
+
+
+  try {
+    const response = await apiService.deleteProperty(`/api/properties/${id}reservation//delete`);
+    if (response.success) {
+      toast.success("Reservation deleted successfully!", {
+        position: "top-center",
+        autoClose: 2000,
+      });
+    } else {
+      toast.error("Failed to delete reservation. Please try again.", {
+        position: "top-center",
+      });
+    }
+  } catch (error) {
+    console.error("Failed to delete reservation", error);
+    toast.error("Failed to delete reservation. Please try again.");
+  }
+};
+
+export default deleteReservation;

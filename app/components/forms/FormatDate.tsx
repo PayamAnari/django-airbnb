f5190a2a8386e6480d50e75f398ff612f99de5f7
PayamAnari <anari.p62@gmail.com>
2024-07-04 20:07:05 +0200

    Correction

--- app/components/forms/FormatDate.tsx
+++ app/components/forms/FormatDate.tsx
@@ -1,5 +1,6 @@
-import { differenceInDays, differenceInMonths, differenceInYears } from 'date-fns';
+import { differenceInDays, differenceInMonths, differenceInYears, differenceInHours, differenceInMinutes } from 'date-fns';
 import { format } from 'date-fns';
+
 
 
 
@@ -10,16 +11,22 @@ export const formatDate = (date) => {
   const daysDifference = differenceInDays(now, inputDate);
   const monthsDifference = differenceInMonths(now, inputDate);
   const yearsDifference = differenceInYears(now, inputDate);
+  const hoursDifference = differenceInHours(now, inputDate);
+  const minutesDifference = differenceInMinutes(now, inputDate);
   const weeksDifference = Math.floor(daysDifference / 7);
-  
+
   if (yearsDifference > 0) {
     return `${yearsDifference} year${yearsDifference > 1 ? 's' : ''}`;
   } else if (monthsDifference > 0) {
     return `${monthsDifference} month${monthsDifference > 1 ? 's' : ''}`;
   } else if (weeksDifference > 0) {
     return `${weeksDifference} week${weeksDifference > 1 ? 's' : ''}`;
-  } else {
+  } else if (daysDifference > 0) {
     return `${daysDifference} day${daysDifference > 1 ? 's' : ''}`;
+  } else if (hoursDifference > 0) {
+    return `${hoursDifference} hour${hoursDifference > 1 ? 's' : ''}`;
+  } else {
+    return `${minutesDifference} minute${minutesDifference > 1 ? 's' : ''}`;
   }
 };
 
